b83b2a150c4a8eb9ab97b03c8034c214b9c85325
Psycadelik <Francisadrian9@hotmail.com>
2018-07-02 05:19:00 -0400

    added exception handler checks

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -52,6 +52,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // Convert all non-http exceptions to a proper 500 http exception
+        // if we don't do this exceptions are shown as a default template
+        // instead of our own view in resources/views/errors/500.blade.php
+//        if ($this->shouldReport($exception) && !$this->isHttpException($exception) && !config('app.debug')) {
+//            $exception = new HttpException(500, 'Whoops!');
+//        }
+
         return parent::render($request, $exception);
     }
 }
